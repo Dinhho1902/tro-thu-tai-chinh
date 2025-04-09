6cbb59a85b708060dba94faa50c8a4c4296b6d1f
Dinhho1902 <dinhthong1902@gmail.com>
2025-04-09 17:18:47 +0700

    Update finance_agent.py

--- finance_agent.py
+++ finance_agent.py
@@ -9,10 +9,10 @@ st.title("AI Personal Finance Planner 💰")
 st.caption("Manage your finances with AI Personal Finance Manager by creating personalized budgets, investment plans, and savings strategies using GPT-4o")
 
 # Get OpenAI API key from user
-openai_api_key = st.text_input("Enter OpenAI API Key to access GPT-4o", type="password")
+openai_api_key = st.write("DB username:", st.secrets["db_username"])
 
 # Get SerpAPI key from the user
-serp_api_key = st.text_input("Enter Serp API Key for Search functionality", type="password")
+serp_api_key = st.write("DB password:", st.secrets["db_password"])
 
 if openai_api_key and serp_api_key:
     researcher = Agent(
